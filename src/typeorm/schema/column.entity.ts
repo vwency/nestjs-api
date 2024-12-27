e5ef6736351fc770d50c060f2e17b7419ced2ba7
vwency <duckdodo666@gmail.com>
2024-12-28 00:52:55 +0600

    feat: typeorm restore

---
+++ src/typeorm/schema/column.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Entity,
+  Column as TypeOrmColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('columns')
+export class Columns {
+  @PrimaryGeneratedColumn('uuid')
+  column_id: string;
+
+  @TypeOrmColumn('uuid')
+  user_id: uuidv4;
+
+  @TypeOrmColumn()
+  column_name: string;
+
+  @TypeOrmColumn()
+  description: string;
+}
---
+++ src/typeorm/schema/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('users')
+export class Users {
+  @PrimaryGeneratedColumn('uuid')
+  user_id: uuidv4;
+
+  @Column({ type: 'varchar', nullable: true })
+  username: string;
+
+  @Column('varchar')
+  hash: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  hashedRt: string;
+}
