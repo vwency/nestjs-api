f391f3a6a4318099fd35f8237fbfa874c6340ded
vwency <duckdodo666@gmail.com>
2024-12-31 03:30:02 +0600

    refactor: local guard

--- src/auth/guards/local.guard.ts
+++ src/auth/guards/local.guard.ts
@@ -1,13 +1,9 @@
-import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
-import { Observable } from 'rxjs'
 
 @Injectable()
 export class LocalGuard extends AuthGuard('local') {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log('Hello, World')
-    return super.canActivate(context)
+  constructor() {
+    super()
   }
 }
