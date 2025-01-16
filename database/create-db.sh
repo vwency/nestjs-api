444194454c35241d3544a35cea3d66d9bf23bdd1
vwency <duckdodo666@gmail.com>
2025-01-16 06:17:57 +0600

    fix: create database in docker

---
+++ database/create-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+until pg_isready -U "$POSTGRES_USER"; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 1
+done
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password <<-EOSQL
+    CREATE DATABASE pure;
+EOSQL
